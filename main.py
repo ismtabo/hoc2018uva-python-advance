b295c7be01fa9114ec6a4d888f66c1ff6117a195
Ismael Taboada Rodero <ismael.jtr@gmail.com>
2018-11-26 23:18:00 +0100

    Add nametuple structure of Comment

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from datetime import datetime
+from collections import namedtuple
 
 from flask import Flask, render_template
 app = Flask(__name__)
 
+Comment = namedtuple('Comment', ['title', 'content', 'datetime'])
 
 @app.route('/')
 def hello_world():
-    comment = "First comment", "This page isn't bad at all.", datetime.utcnow()
+    comment = Comment("First comment", "This page isn't bad at all.", datetime.utcnow())
     return render_template('index.html', comment=comment)

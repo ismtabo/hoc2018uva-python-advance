0e79f76d6e9b6b9728db2a954389ccb7182ea69c
Ismael Taboada Rodero <ismaeljose.taboada@uva.es>
2018-11-27 14:16:54 +0100

    Add Vuejs

--- static/js/main.js
+++ static/js/main.js
@@ -1,65 +1,64 @@
 const socket = io();
 
-axios.get('/comments', {
-	author: document.getElementById('author').value,
-	title: document.getElementById('title').value,
-	content: document.getElementById('content').value
-}).then(function (response) {
-	let comments = response.data.comments;
-	for (let comment of comments) {
-		add_comment(comment);
-	}
-})
+Vue.config.devtools = true;
 
-function update_users(users) {
-	document.getElementById('users').innerHTML = '' + users;
-}
-
-socket.on('new connection', function (data) {
-	update_users(data.users);
-});
-
-socket.on('new disconnection', function (data) {
-	update_users(data.users);
-});
-
-socket.on('new comment', function (data) {
-	add_comment(data.comment);
-})
-
-function add_comment(comment) {
-	let comment_list = document.getElementById('comment-list');
-	let users_span = document.getElementById('users-span');
-
-	let li = document.createElement('li');
-	li.innerHTML = `<a href="#" class="comment list-group-item list-group-item-action flex-column align-items-start">
-					<div class="d-flex w-100 justify-content-between">
-						<h5 class="mb-1">${comment.title}</h5>
-						<small class="mb-1">${comment.author}</small>
-					</div>
-					<p class="mb-1">${comment.content}</p>
-					<div class="d-flex flex-row justify-content-end">
-						<small class="mb-1">${moment(comment.datetime).fromNow()}</small>
-					</div>
-				</a>`;
-
-	comment_list.insertBefore(li, users_span);
-}
-
-document.getElementById('comment-form').addEventListener('submit',
-	function (event) {
-		axios.post('/comments', {
-			author: document.getElementById('author').value,
-			title: document.getElementById('title').value,
-			content: document.getElementById('content').value
-		}).then(function (response) {
-			document.getElementById('comment-form').reset();
-			toastr.success('Your comment has been submited.', 'Great!', { timeOut: 5000 });
-		}).catch(function (error) {
-			toastr.error('Your comment has not been submited.', 'Warning!', { timeOut: 5000 });
+var app = new Vue({
+	el: "#app",
+	delimiters: ["[[", "]]"],
+	data: {
+		users: 0,
+		comments: [],
+		author: "",
+		title: "",
+		content: ""
+	},
+	created() {
+		axios.get("/comments", {
+			author: document.getElementById("author").value,
+			title: document.getElementById("title").value,
+			content: document.getElementById("content").value
+		}).then((response) => {
+			this.comments = response.data.comments;
+		}).catch((error) => {
 			console.error(error);
 		});
 
-		event.preventDefault();
+		socket.on("new connection", (data) => {
+			this.users = data.users;
+		});
+
+		socket.on("new disconnection", (data) => {
+			this.users = data.users;
+		});
+
+		socket.on("new comment", (data) => {
+			this.comments.push(data.comment);
+		});
+	},
+	methods: {
+		onSubmitComment(event) {
+			axios.post("/comments", {
+				author: this.author,
+				title: this.title,
+				content: this.content
+			}).then(() => {
+				this.resetForm();
+				event.target.reset();
+				toastr.success("Your comment has been submited.", "Great!", {
+					timeOut: 5000
+				});
+			}).catch((error) => {
+				toastr.error("Your comment has not been submited.", "Warning!", {
+					timeOut: 5000
+				});
+				console.error(error);
+			});
+
+		},
+		resetForm() {
+			this.author = '';
+			this.title = '';
+			this.content = '';
+		}
 	}
-);
+});
